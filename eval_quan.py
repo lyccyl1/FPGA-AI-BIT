d8f200df9dbaeee254e6ba5148e7f21cc942d429
Eason Leo <112943817+lyccyl1@users.noreply.github.com>
2023-08-24 20:23:41 +0800

    Add files via upload

---
+++ eval_quan.py
@@ -0,0 +1,66 @@
+import torch
+import torch.quantization
+import numpy as np 
+from model import Model  # 导入你的模型类
+import os 
+from torchvision.datasets import mnist
+from torch.utils.data import DataLoader
+from torchvision.transforms import ToTensor
+from torch.nn import Module
+from torch import nn
+
+
+def print_size_of_model(model):
+    torch.save(model.state_dict(), "temp.p")
+    print('Size (MB):', os.path.getsize("temp.p")/1e6)
+    os.remove('temp.p')
+model = Model()
+model = torch.load("models/mnist0.977.pth")
+print_size_of_model(model)
+model.eval()  # 量化前，需要确保模型处于 eval 模式
+quantized_model = torch.quantization.quantize_dynamic(model, {torch.nn.Linear}, dtype=torch.qint8)
+
+
+print_size_of_model(quantized_model)
+# torch.jit.save(quantized_model, 'models/mnist_quan.pt')
+# -- 量化前后的精度对比 --
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+batch_size = 256
+train_dataset = mnist.MNIST(root='./data', train=True, transform=ToTensor())
+test_dataset = mnist.MNIST(root='./data', train=False, transform=ToTensor())
+train_loader = DataLoader(train_dataset, batch_size=batch_size)
+test_loader = DataLoader(test_dataset, batch_size=batch_size)
+all_correct_num = 0
+all_sample_num = 0
+for idx, (test_x, test_label) in enumerate(test_loader):
+    test_x = test_x.to(device)
+    test_label = test_label.to(device)
+    predict_y = model(test_x.float()).detach()
+    predict_y =torch.argmax(predict_y, dim=-1)
+    current_correct_num = predict_y == test_label
+    all_correct_num += np.sum(current_correct_num.to('cpu').numpy(), axis=-1)
+    all_sample_num += current_correct_num.shape[0]
+print('accuracy: {:.6f}'.format(all_correct_num / all_sample_num), flush=True)
+
+
+
+all_correct_num = 0
+all_sample_num = 0
+# print(1)
+quantized_model = torch.load("models/mnist_quan0.977.pth")
+# model = quanModel()
+# device = 'cpu'
+quantized_model.eval()
+quantized_model.to(device)
+# print(1)
+with torch.no_grad():
+    for idx, (test_x, test_label) in enumerate(test_loader):
+        test_x = test_x.to(device)
+        test_label = test_label.to(device)
+        predict_y = quantized_model(test_x.float()).detach()
+        predict_y =torch.argmax(predict_y, dim=-1)
+        current_correct_num = predict_y == test_label
+        all_correct_num += np.sum(current_correct_num.to('cpu').numpy(), axis=-1)
+        all_sample_num += current_correct_num.shape[0]
+
+print('accuracy: {:.6f}'.format(all_correct_num / all_sample_num), flush=True)
---
+++ model.py
@@ -0,0 +1,145 @@
+from torch.nn import Module
+from torch import nn
+import torch
+
+class Model(Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(256, 120)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(120, 84)
+        self.relu4 = nn.ReLU()
+        self.fc3 = nn.Linear(84, 10)
+        self.relu5 = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.conv1(x)
+        y = self.relu1(y)
+        y = self.pool1(y)
+        y = self.conv2(y)
+        y = self.relu2(y)
+        y = self.pool2(y)
+        y = y.view(y.shape[0], -1)
+        y = self.fc1(y)
+        y = self.relu3(y)
+        y = self.fc2(y)
+        y = self.relu4(y)
+        y = self.fc3(y)
+        y = self.relu5(y)
+        return y
+    
+    
+class CNNModel(nn.Module):
+    def __init__(self):
+        super(CNNModel, self).__init__()
+        
+        # Convolution layer 1 (（w - f + 2 * p）/ s ) + 1
+        self.conv1 = nn.Conv2d(in_channels = 1 , out_channels = 32, kernel_size = 5, stride = 1, padding = 0 )
+        self.relu1 = nn.ReLU()
+        self.batch1 = nn.BatchNorm2d(32)
+        
+        self.conv2 = nn.Conv2d(in_channels =32 , out_channels = 32, kernel_size = 5, stride = 1, padding = 0 )
+        self.relu2 = nn.ReLU()
+        self.batch2 = nn.BatchNorm2d(32)
+        self.maxpool1 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        self.conv1_drop = nn.Dropout(0.25)
+
+        # Convolution layer 2
+        self.conv3 = nn.Conv2d(in_channels = 32, out_channels = 64, kernel_size = 3, stride = 1, padding = 0 )
+        self.relu3 = nn.ReLU()
+        self.batch3 = nn.BatchNorm2d(64)
+        
+        self.conv4 = nn.Conv2d(in_channels = 64, out_channels = 64, kernel_size = 3, stride = 1, padding = 0 )
+        self.relu4 = nn.ReLU()
+        self.batch4 = nn.BatchNorm2d(64)
+        self.maxpool2 = nn.MaxPool2d(kernel_size = 2, stride = 2)
+        self.conv2_drop = nn.Dropout(0.25)
+
+        # Fully-Connected layer 1
+        
+        self.fc1 = nn.Linear(576,256)
+        self.fc1_relu = nn.ReLU()
+        self.dp1 = nn.Dropout(0.5)
+        
+        # Fully-Connected layer 2
+        self.fc2 = nn.Linear(256,10)
+                
+    def forward(self, x):
+        # conv layer 1 的前向计算，3行代码
+        out = self.conv1(x)
+        out = self.relu1(out)
+        out = self.batch1(out)
+        
+        out = self.conv2(out)
+        out = self.relu2(out)
+        out = self.batch2(out)
+        
+        out = self.maxpool1(out)
+        out = self.conv1_drop(out)
+
+        # conv layer 2 的前向计算，4行代码
+        out = self.conv3(out)
+        out = self.relu3(out)
+        out = self.batch3(out)
+        
+        out = self.conv4(out)
+        out = self.relu4(out)
+        out = self.batch4(out)
+        
+        out = self.maxpool2(out)
+        out = self.conv2_drop(out)
+
+        #Flatten拉平操作
+        out = out.view(out.size(0),-1)
+
+        #FC layer的前向计算（2行代码）
+        out = self.fc1(out)
+        out = self.fc1_relu(out)
+        out = self.dp1(out)
+        
+        out = self.fc2(out)
+
+        return out
+    
+    
+class quanModel(Module):
+    def __init__(self):
+        super(quanModel, self).__init__()
+        self.quant = torch.quantization.QuantStub()
+        self.dequant = torch.quantization.DeQuantStub()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.relu1 = nn.ReLU()
+        self.pool1 = nn.MaxPool2d(2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(2)
+        self.fc1 = nn.Linear(256, 120)
+        self.relu3 = nn.ReLU()
+        self.fc2 = nn.Linear(120, 84)
+        self.relu4 = nn.ReLU()
+        self.fc3 = nn.Linear(84, 10)
+        self.relu5 = nn.Sigmoid()
+
+    def forward(self, x):
+        x = self.quant(x)
+        y = self.conv1(x)
+        y = self.relu1(y)
+        y = self.pool1(y)
+        y = self.conv2(y)
+        y = self.relu2(y)
+        y = self.pool2(y)
+        y = y.view(y.shape[0], -1)
+        y = self.fc1(y)
+        y = self.relu3(y)
+        y = self.fc2(y)
+        y = self.relu4(y)
+        y = self.fc3(y)
+        y = self.relu5(y)
+        y = self.dequant(y)
+        return y
