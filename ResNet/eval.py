a9e0d213ebb9a9358aa0600eb9538dd0c775e457
Enue Lee <94110802+Programming-D@users.noreply.github.com>
2023-09-06 01:00:58 +0800

    Add files via upload

---
+++ ResNet/eval.py
@@ -0,0 +1,68 @@
+import numpy as np
+from torchvision.models import resnet152, resnet50
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from torchvision.transforms import transforms
+from torchvision.datasets import CIFAR10
+from tqdm import tqdm
+
+device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+
+def evaluate(model, data_loader):
+    model.eval()
+    pred_tags = []
+    true_tags = []
+    with torch.no_grad():
+        for batch in data_loader:
+            batch_data = batch[0].to(device)
+            batch_label = batch[1].to(device)
+
+            logits = model(batch_data)
+
+            pred = torch.argmax(logits, dim=1).cpu().numpy()
+            tags = batch_label.cpu().numpy()
+
+            pred_tags.extend(pred)
+            true_tags.extend(tags)
+
+    assert len(pred_tags) == len(true_tags)
+    correct_num = sum(int(x == y) for (x, y) in zip(pred_tags, true_tags))
+    accuracy = correct_num / len(pred_tags)
+
+    return accuracy
+
+state = torch.load("./models/resnet50_best.pt")
+model = resnet50()
+model.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
+model.fc = nn.Linear(2048, 10)
+model.load_state_dict(state)
+model.to(device)
+
+
+transform_train = transforms.Compose([
+    transforms.RandomCrop(32, padding=4), #padding后随机裁剪
+    transforms.Grayscale(),
+    transforms.RandomHorizontalFlip(0.5),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4734), (0.2507)),
+])
+ 
+transform_test = transforms.Compose([
+    transforms.Grayscale(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4734), (0.2507)),
+])
+
+
+torch.manual_seed(0)
+g = torch.Generator()
+train_dataloader = DataLoader(CIFAR10(root='./cifar-10-python/', train=True, transform=transform_train),
+                                        shuffle=True, generator=g, batch_size=32, num_workers=4)
+test_dataloader = DataLoader(CIFAR10(root='./cifar-10-python/', train=False, transform=transform_test),
+                            batch_size=32, num_workers=4)
+
+test_acc = evaluate(model, test_dataloader)
+print(f"Resnet50 test acc is {test_acc}")
